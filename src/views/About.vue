a2204c3cf11742bdfadc709b16e25e899b402925
Markus Lautenbach <markus.lautenbach@gmail.com>
2020-04-02 22:51:25 +0200

    Remove facebook survivor

--- src/views/About.vue
+++ src/views/About.vue
@@ -16,10 +16,10 @@
             <p><span class="headline">{{ $t('about.headDataUpdate') }}</span> {{ $t('about.dataUpdate') }}</p>
             <p><span class="headline">{{ $t('about.headPublicData') }}</span> {{ $t('about.publicData') }}</p>
             <p><span class="headline">{{ $t('about.headDataCare') }}</span> {{ $t('about.dataCare') }}</p>
-            <p><span class="headline">{{ $t('about.headImprovement') }}</span> {{ $t('about.improvement') }}</p>
-            <p><span class="headline">{{ $t('about.supportHeader') }}</span> {{ $t('about.support') }} <a
+            <p><span class="headline">{{ $t('about.headImprovement') }}</span> {{ $t('about.improvement') }} <a
               href="mailto:covidmap.sweden@gmail.com" target="_blank"
-            >covidmap.sweden@gmail.com</a>.</p>
+            >covidmap.sweden@gmail.com</a</p>
+            <p><span class="headline">{{ $t('about.supportHeader') }}</span> {{ $t('about.support') }} </p>
             <p>{{ $t('about.source1') }} <a
               href="https://github.com/ch-covid-19" target="_blank"
             >{{$t('about.link')}}</a>{{ $t('about.source2') }} </p>
